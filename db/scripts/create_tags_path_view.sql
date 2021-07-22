49d0c4538d10d1463f2befc7fd1260e22535b8fc
Jerry Tohvan <jerrytohvan@gmail.com>
2021-07-22 12:44:18 +0800

    Fix InvalidQuery

--- db/scripts/create_tags_path_view.sql
+++ db/scripts/create_tags_path_view.sql
@@ -1,6 +1,5 @@
-create view tags_paths as
-       WITH RECURSIVE tag_path (id, created_at, updated_at, community_id, tag_set_id, wiki_markdown,
-                     wiki, excerpt, parent_id, name, path) AS
+create view tags_paths AS
+       WITH RECURSIVE tag_path(id, created_at, updated_at, community_id, tag_set_id, wiki_markdown, wiki, excerpt, parent_id, name, path) AS
               (
                      SELECT id, created_at, updated_at, community_id, tag_set_id, wiki_markdown,
                             wiki, excerpt, parent_id, name, name as path
@@ -11,5 +10,4 @@ create view tags_paths as
                             t.wiki, t.excerpt, t.parent_id, t.name, concat(tp.path, ' > ', t.name) as path
                      FROM tag_path AS tp JOIN tags AS t ON tp.id = t.parent_id
               )
-SELECT * FROM tag_path
-ORDER BY path;
+SELECT * FROM tag_path ORDER BY path;
