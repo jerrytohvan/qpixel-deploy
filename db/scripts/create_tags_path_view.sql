4508ab9e9cf2d7bdf3a674f4b0c95c30e9e5e66b
Jerry Tohvan <jerrytohvan@gmail.com>
2021-07-22 17:50:06 +0800

    Format SQL

--- db/scripts/create_tags_path_view.sql
+++ db/scripts/create_tags_path_view.sql
@@ -1,13 +1,43 @@
-create view tags_paths AS
-       WITH RECURSIVE tag_path(id, created_at, updated_at, community_id, tag_set_id, wiki_markdown, wiki, excerpt, parent_id, name, path) AS
-              (
-                     SELECT id, created_at, updated_at, community_id, tag_set_id, wiki_markdown,
-                            wiki, excerpt, parent_id, name, name as path
-                     FROM tags
-                     WHERE parent_id IS NULL
-                     UNION ALL
-                     SELECT t.id, t.created_at, t.updated_at, t.community_id, t.tag_set_id, t.wiki_markdown,
-                            t.wiki, t.excerpt, t.parent_id, t.name, concat(tp.path, ' > ', t.name) as path
-                     FROM tag_path AS tp JOIN tags AS t ON tp.id = t.parent_id
-              )
-SELECT * FROM tag_path ORDER BY path;
+create view tags_paths AS WITH RECURSIVE tag_path(id, created_at, updated_at, community_id, tag_set_id, wiki_markdown, wiki, excerpt, parent_id, name, path) AS
+(
+   SELECT
+      id,
+      created_at,
+      updated_at,
+      community_id,
+      tag_set_id,
+      wiki_markdown,
+      wiki,
+      excerpt,
+      parent_id,
+      name,
+      name as path
+   FROM
+      tags
+   WHERE
+      parent_id IS NULL
+   UNION ALL
+   SELECT
+      t.id,
+      t.created_at,
+      t.updated_at,
+      t.community_id,
+      t.tag_set_id,
+      t.wiki_markdown,
+      t.wiki,
+      t.excerpt,
+      t.parent_id,
+      t.name,
+      concat(tp.path, ' > ', t.name) as path
+   FROM
+      tag_path AS tp
+      JOIN
+         tags AS t
+         ON tp.id = t.parent_id
+)
+SELECT
+   *
+FROM
+   tag_path
+ORDER BY
+   path;
