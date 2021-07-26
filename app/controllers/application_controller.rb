3f2fbeae4fcf15e913364cade403a9e310a73a8c
Jerry Tohvan <jerrytohvan@gmail.com>
2021-07-26 22:32:03 +0800

    add  `skip_forgery_protection`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  skip_forgery_protection
+  # protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_globals
   before_action :check_if_warning_or_suspension_pending
