6366945200ef2d35a52e6c47ea1351f44641287f
Jerry Tohvan <jerrytohvan@gmail.com>
2021-07-27 12:35:42 +0800

    Edit `:null_session`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # skip_before_filter :verify_authenticity_token
   # protect_from_forgery prepend: true
-    protect_from_forgery with :null_session
-
+  protect_from_forgery :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_globals
   before_action :check_if_warning_or_suspension_pending
