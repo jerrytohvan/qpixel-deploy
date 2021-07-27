496ad4cd1498c1ae4612e39e0848c395466f49b4
Jerry Tohvan <jerrytohvan@gmail.com>
2021-07-27 12:55:47 +0800

    Remove domain server cache

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: 'codidact_acct', domain: :all
+  Rails.application.config.session_store :cookie_store, key: 'codidact_acct'
 else
   Rails.application.config.session_store :cookie_store, key: 'codidact_acct'
 end
